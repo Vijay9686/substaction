c395b52cf8c80f12429eff94d244d7d2723d8374
Vijay9686 <105700592+Vijay9686@users.noreply.github.com>
2022-06-13 22:12:01 +0530

    Add files via upload

---
+++ substraction.java
@@ -0,0 +1,19 @@
+package vijay;
+
+public class substraction 
+{
+
+	public static void main(String[] args) 
+	{
+		int a= 10;
+		int b=5;
+		int c;
+		
+			c = a - b;
+			System.out.println("The Result is "+c);
+
+		
+
+	}
+
+}
